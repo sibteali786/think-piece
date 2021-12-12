e8f432f16e778751c36fc904d39e819ebf041a04
Syed_Sibteali_Baqar <ssibteali.ce41ceme@ce.ceme.edu.pk>
2021-12-12 20:39:27 +0500

    added functionality to add custom user profile data

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { auth, createUserProfileDocument } from '../firebase';
 
 class SignUp extends Component {
   state = { displayName: '', email: '', password: '' };
@@ -9,10 +10,16 @@ class SignUp extends Component {
     this.setState({ [name]: value });
   };
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
-
+    const {email, password, displayName} = this.state;
     this.setState({ displayName: '', email: '', password: '' });
+    try {
+      const {user} = await auth.createUserWithEmailAndPassword(email,password);
+      createUserProfileDocument(user,{displayName});
+    } catch(error) {
+      console.error(error);
+    }
   };
 
   render() {
