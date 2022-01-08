c93f93a6c7b9ccac36784ce099440c8f0b1f29a2
Syed_Sibteali_Baqar <ssibteali.ce41ceme@ce.ceme.edu.pk>
2022-01-08 21:34:15 +0500

    getAllposts function made ( but not deployed due to payment issues )

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,18 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
+admin.initializeApp(functions.config().firebase);
+const firestore = admin.firestore();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+exports.getAllPosts = functions.https.onRequest(async (resquest,response)=>{
+    const snapshot = await firestore.collection('posts').get();
+    const posts = snapshot.docs.map(doc=>({id:doc.id, ...doc.data()}));
+    response.json({posts});
+})
