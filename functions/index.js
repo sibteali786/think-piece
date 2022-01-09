cee1db3166bfeb3163f0b0bedf61e482ea964489
Syed_Sibteali_Baqar <ssibteali.ce41ceme@ce.ceme.edu.pk>
2022-01-09 13:07:41 +0500

    added cloud fuction sanitizeContent

--- functions/index.js
+++ functions/index.js
@@ -15,4 +15,23 @@ exports.getAllPosts = functions.https.onRequest(async (resquest,response)=>{
     const snapshot = await firestore.collection('posts').get();
     const posts = snapshot.docs.map(doc=>({id:doc.id, ...doc.data()}));
     response.json({posts});
-})
+});
+
+exports.sanitizeContent = functions.firestore.document(`posts/{postId}`).onWrite(async (change)=>{
+    if (!change.after.exists) return;   // doc will not exists if its deleted so it cover both update and create as well
+    
+    const {content , sanitized } = change.after.data();   // change is just like the snapshot 
+    // sanitized is jut some thing which if true can trigger this condition and return something otherwise it returns null and we can get out of 
+    // recursion  
+    /// These change functions should always return something otherwise they create errors and problems like recursion
+    if(change && !sanitized){
+        return change.after.ref.update({
+            content: content.replace(/CofeeScript/g,"**********" ),
+            sanitized: true,
+        })
+    } 
+
+    // if there was no content just return null
+    return null;
+});
+
